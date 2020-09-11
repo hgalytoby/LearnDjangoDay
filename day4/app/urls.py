7872d74f6a2196f06891502f23cdd100df6cff39
hgalytoby <hgalytoby@gmail.com>
2020-09-12 01:51:13 +0800

    學習Django

--- day4/app/urls.py
+++ day4/app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from app import views
 
 app_name = 'app'
-urlpatterns = {
-    path('hello/', views.hello)
-}
+urlpatterns = [
+    path('hello/', views.hello, name='hello'),
+    path('get_ticket/', views.get_ticket, name='get_ticket'),
+    path('get_info/', views.get_info, name='get_info'),
+]
--- day4/app/views.py
+++ day4/app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse
+
+
+def hello(request):
+    response = HttpResponse()
+    # response.content = 'hello, django'
+    # response.status_code = 404
+    response.write('用這個記得要用flush')
+    response.flush()
+    return response
+
+
+def get_ticket(request):
+    url = reverse('app:hello')
+    # return HttpResponseRedirect(url)
+    return redirect(url)
+
+
+def get_info(request):
+    return JsonResponse({'info': 'get ok'})
