c1e3e568069bf7a5eb1b1e3631310a8bd00a7e3d
hgalytoby <hgalytoby@gmail.com>
2020-09-13 05:11:47 +0800

    學習Django

--- day4/app/urls.py
+++ day4/app/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('hello/', views.hello, name='hello'),
     path('get_ticket/', views.get_ticket, name='get_ticket'),
     path('get_info/', views.get_info, name='get_info'),
+    path('set_cookie/', views.set_cookie, name='set_cookie'),
+    path('get_cookie/', views.get_cookie, name='get_cookie'),
+    path('login/', views.login, name='login'),
+    path('do_login/', views.do_login, name='do_login'),
+    path('mine/', views.mine, name='mine'),
 ]
--- day4/app/views.py
+++ day4/app/views.py
@@ -22,3 +22,39 @@ def get_ticket(request):
 
 def get_info(request):
     return JsonResponse({'info': 'get ok'})
+
+
+def set_cookie(request):
+    response = HttpResponse('設定 Cookie')
+    response.set_cookie('user', 'dudulu')
+    response.set_cookie('pass', '123')
+    return response
+
+
+def get_cookie(request):
+    cookies = request.COOKIES
+    import json
+
+    # print(json.dumps(cookies))
+    return HttpResponse(json.dumps(cookies))
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def do_login(request):
+    user_name = request.POST.get('user_name')
+    response = HttpResponse('登入成功')
+    # response.set_cookie(key='user_name', value=user_name, max_age=60)
+    response.set_signed_cookie('content', user_name, 'dudulu')
+    return response
+    # return redirect(reverse('app:mine'))
+
+
+def mine(request):
+    # user_name = request.COOKIES.get('user_name')
+    user_name = request.get_signed_cookie('coontent', salt='user_name')
+    if user_name:
+        return HttpResponse(user_name)
+    return redirect(reverse('app:login'))
